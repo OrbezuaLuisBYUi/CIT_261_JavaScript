da084717e7c32e3fd0a0afce3cbeee3c588f6e48
OrbezuaLuisBYUi <orb13002@byui.edu>
2019-11-07 12:28:33 -0700

    updateTopic 5

--- js/week05a.js
+++ js/week05a.js
@@ -32,3 +32,103 @@ function resetList() {
     document.getElementById("deleteListNow").innerHTML = "You just deleted all the items from your List" + "<br>";
 
 }
+
+function assoativearray() {
+    // our Object
+    var movies = {
+        "0": {
+            "title": "la momia",
+            "characteristic": "terror"
+        },
+
+        "1":{
+            "title": "Terminator",
+            "characteristic": "action"
+        },
+
+        "2":{
+            "title": "Dracula",
+            "characteristic": "terror"
+        },
+
+        "3":{
+            "title": "The Dog",
+            "characteristic": "comedy"
+        },
+
+        "4":{
+            "title": "The spy",
+            "characteristic": "fiction"
+        }
+    };
+
+    var theCollectionMovies = "";
+    for (var i in movies) { // i is the index for the Json
+        var title = "";
+        var char = "";
+        for (var j in movies[i]) // j is the values , columns
+        {
+            theCollectionMovies += " Position: " +i+ " KEY: "+j+" VALUE: "+movies[i][j]+"<br>";
+            if(j == "title")
+            {
+                title = movies[i][j];
+            }
+            if(j == "characteristic")
+            {
+                char = movies[i][j];
+            }
+        }
+        // our array
+        if(i == 0){ var movies1 = [title, char]; }
+        if(i == 1){ var movies2 = [title, char]; }
+        if(i == 2){ var movies3 = [title, char]; }
+        if(i == 3){ var movies4 = [title, char]; }
+        if(i == 4){ var movies5 = [title, char]; }
+    }
+
+    // storing our array as a string
+    localStorage.setItem("theCollection", theCollectionMovies);
+    localStorage.setItem("theMovie1", JSON.stringify(movies1));
+    localStorage.setItem("theMovie2", JSON.stringify(movies2));
+    localStorage.setItem("theMovie3", JSON.stringify(movies3));
+    localStorage.setItem("theMovie4", JSON.stringify(movies4));
+    localStorage.setItem("theMovie5", JSON.stringify(movies5));
+    // retrieving our data and converting it back into an array
+    var resultCollection = localStorage.getItem("theCollection");
+    var localStorageMovie1 = localStorage.getItem("theMovie1");
+    var localStorageMovie2 = localStorage.getItem("theMovie2");
+    var localStorageMovie3 = localStorage.getItem("theMovie3");
+    var localStorageMovie4 = localStorage.getItem("theMovie4");
+    var localStorageMovie5 = localStorage.getItem("theMovie5");
+    var result = "<strong>DATA OBJECT:</strong><br> "+resultCollection+"<br><br><strong>ARRAYS</strong><br>MOVIE1"+localStorageMovie1+"<br>MOVIE2"+localStorageMovie2+"<br>MOVIE3"+localStorageMovie3+"<br>MOVIE4"+localStorageMovie4+"<br>MOVIE5"+localStorageMovie5;
+
+    var div = document.getElementById("resultArrayObject").innerHTML = result;
+}
+function removeItemArray()
+{
+    var removeItemMovie = prompt("Do you What delete 1,2,3,4,5?");
+    if(removeItemMovie == 1){ localStorage.removeItem('theMovie1'); }
+    if(removeItemMovie == 2){ localStorage.removeItem('theMovie2'); }
+    if(removeItemMovie == 3){ localStorage.removeItem('theMovie3'); }
+    if(removeItemMovie == 4){ localStorage.removeItem('theMovie4'); }
+    if(removeItemMovie == 5){ localStorage.removeItem('theMovie5'); }
+    seeObjectandArray();
+}
+function seeObjectandArray()
+{
+    // retrieving our data and converting it back into an array
+    var resultCollection = localStorage.getItem("theCollection");
+    var localStorageMovie1 = localStorage.getItem("theMovie1");
+    var localStorageMovie2 = localStorage.getItem("theMovie2");
+    var localStorageMovie3 = localStorage.getItem("theMovie3");
+    var localStorageMovie4 = localStorage.getItem("theMovie4");
+    var localStorageMovie5 = localStorage.getItem("theMovie5");
+    if(localStorageMovie1 == null){ localStorageMovie1 = "This movie was deleted"; }
+    if(localStorageMovie2 == null){ localStorageMovie2 = "This movie was deleted"; }
+    if(localStorageMovie3 == null){ localStorageMovie3 = "This movie was deleted"; }
+    if(localStorageMovie4 == null){ localStorageMovie4 = "This movie was deleted"; }
+    if(localStorageMovie5 == null){ localStorageMovie5 = "This movie was deleted"; }
+    var result = "<strong>DATA OBJECT:</strong><br> "+resultCollection+"<br><br><strong>ARRAYS</strong><br>MOVIE1"+localStorageMovie1+"<br>MOVIE2"+localStorageMovie2+"<br>MOVIE3"+localStorageMovie3+"<br>MOVIE4"+localStorageMovie4+"<br>MOVIE5"+localStorageMovie5;
+
+    var div = document.getElementById("resultArrayObject").innerHTML = result;
+}
